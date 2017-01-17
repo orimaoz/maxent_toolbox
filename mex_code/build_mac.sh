54ee9073395aba7935c39d0856e137afb2b2d2a9
Ori Maoz <ori.maoz@weizmann.ac.il>
2017-01-17 12:44:59 +0200

    Simplified the build process in unix/mac. Single build which makes a binary with two execution paths (one with AVX2, one backwards-compatible to SSE4.2)

--- mex_code/build_mac.sh
+++ mex_code/build_mac.sh
@@ -2,35 +2,15 @@
 # Builds the maximum entropy toolbox for MATLAB on MacOS hosts
 # Usage:
 #
-# "source build_mac.sh host" - builds with maximum optimizations for current host
-# "source build_mac.sh compatible" - builds backwards-compatible code for old computers
+# "source build_mac.sh" - builds with the mex code
 
 
 source /opt/intel/compilers_and_libraries_2017/mac/bin/compilervars.sh intel64
 export COMPILERLIBDIR=/opt/intel/compilers_and_libraries_2017/mac/lib
 
-if [ "$1" == "" ]; then
-	echo
-	echo Usage: source build_mac.sh [optimization_type]
-	echo where [optimization_type] is either:
-	echo       optimized 	  - builds with maximum optimizations for current host 
-	echo	   compatible - builds backwards-compatible code for old computers
-	return
-fi
 
-if [ "$1" == "optimized" ]; then
-# The following options file uses the maximum optimizations available for the computer
-# used to build the code:
-	echo Optimizating for current host
-	export OPTIONSFILE="mex_C++_maci64_optimized.xml"
-fi
-
-if [ "$1" == "compatible" ]; then
-# The following options file uses optimizations only up to SSE3, this will result in slower
-# code that can run on older computers (circa 2005 and onwards). Uncomment it if this is what you need.
-	echo Using backwards-compatible optimizations
-	export OPTIONSFILE="mex_C++_maci64_compatible.xml"
-fi
+# The following options file creates two execution branches, one with AVX2 and one with SSE4.2 for older processors
+export OPTIONSFILE="mex_C++_maci64.xml"
 
 export MKLLIB=$MKLROOT/lib
 export IPPLIB=$IPPROOT/lib
--- mex_code/build_unix.sh
+++ mex_code/build_unix.sh
@@ -2,8 +2,7 @@
 # Builds the maximum entropy toolbox for MATLAB on unix hosts
 # Usage:
 #
-# "source build_unix.sh host" - builds with maximum optimizations for current host
-# "source build_unix.sh compatible" - builds backwards-compatible code for old computers
+# "source build_unix.sh" - builds the MEX functions
 
 
 source compilervars.sh intel64
@@ -12,28 +11,7 @@ export MKLLIB=/apps/RH6U4/intel/2016/mkl/lib/intel64
 export IPPLIB=/apps/RH6U4/intel/2016/ipp/lib/intel64
 
 
-if [ "$1" == "" ]; then
-	echo
-	echo Usage: source build_unix.sh [optimization_type]
-	echo where [optimization_type] is either:
-	echo       optimized 	  - builds with maximum optimizations for current host 
-	echo	   compatible - builds backwards-compatible code for old computers
-	return
-fi
-
-if [ "$1" == "optimized" ]; then
-# The following options file uses the maximum optimizations available for the computer
-# used to build the code:
-	echo Optimizating for current host
-	export OPTIONSFILE="mex_g++_glnxa64_optimized.xml"
-fi
-
-if [ "$1" == "compatible" ]; then
-# The following options file uses optimizations only up to SSE3, this will result in slower
-# code that can run on older computers (circa 2005 and onwards). Uncomment it if this is what you need.
-	echo Using backwards-compatible optimizations
-	export OPTIONSFILE="mex_g++_glnxa64_compatible.xml"
-fi
-
+# The following options file uses two execution paths, one with AVX2 for newer CPUs and one with SSE4.2 for older CPUs
+export OPTIONSFILE="mex_g++_glnxa64.xml"
 
 sh ./buildmex.sh
