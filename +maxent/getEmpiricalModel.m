62241d8eb16720c57e1c47555b77c1b33fa205ae
Ori Maoz <orimaoz@gmail.com>
2017-11-09 11:20:43 +0200

    doc fixes for getEmpiricalModel

--- +maxent/getEmpiricalModel.m
+++ +maxent/getEmpiricalModel.m
@@ -6,16 +6,20 @@
 %
 %
 % Arguments (mandatory):
-%   model   - Maximum entropy model as returned by the createModel() function. 
-%   samples - Set of samples to train the model on, in the format (ncells x nsamples). 
-%             If the input dimensionality is small enough for an exhaustive computation, a Boltzmann distribution (in the
-%             same format as returned by createModel) may be inputed instead of a raster, in this case the target
-%             marginals will be computed in an exact manner from the distribution.   
+%   samples - Set of samples, in the format (ncells x nsamples). 
 %
 % Optional arguments (in the form of Name,Value pairs):
 %   min_count        - ignore all samples that appears less than this number of times (default 1)
 %
-% Last update: Ori Maoz 30/06/2016
+% Output:
+%   model_out - structure with several fields describing the empirical distrbution:
+%       model_out.words  - unique codewords in the empirical distribution
+%       model_out.counts - how many times each of these codewords repeated in the empirical dataset
+%       model_out.logprobs - log probabilities of the codewords in the empirical distribution
+%       model_out.logprobs - entropy of the empirical distribution, in bits
+%
+
+% Last update: Ori Maoz 09/11/2016
 function model_out = getEmpiricalModel(samples,varargin)
 
 DEFAULT_MIN_COUNT = 1;
